c022747c74fdcb035114b436efd1ffb759f756e8
HangKeon <whgkdrjs4321@naver.com>
2023-12-21 20:39:34 +0900

    2023/12/21

--- pages/section10/10-03-typescript-utility-type/index.ts
+++ pages/section10/10-03-typescript-utility-type/index.ts
@@ -5,3 +5,16 @@ export interface IProfile{
   hobby?: string
 }
 
+
+
+//1. Partial type => 전부 ?를 추가
+type aaa = Partial<IProfile>
+
+//2. Required type => 전부 ?를 제거
+type bbb = Required<IProfile>
+
+//3. Pick type => 내가 원하는 것만 고름
+type ccc = Pick<IProfile, "name" | "age">
+
+//4. Omit type => 내가 고르는 것만 뺌
+type ddd = Omit<IProfile, "school">
